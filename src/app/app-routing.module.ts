2f52c99ad47f0ae7a3c33c8114983a5a0fb35b88
tinmn9088 <tinman.8.9@yandex.ru>
2023-01-23 21:40:18 +0300

    feat: add `specializations/list` route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpecializationListComponent } from './specialization/components/specialization-list/specialization-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'specializations',
+    children: [{ path: 'list', component: SpecializationListComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
