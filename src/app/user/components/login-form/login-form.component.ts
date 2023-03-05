c00980fe6e80293b1c03680cb34f3f0331475eba
tinmn9088 <ikireyeu@interventioninsights.com>
2023-03-05 21:28:16 +0300

    feat: made login form adaptive

--- src/app/user/components/login-form/login-form.component.ts
+++ src/app/user/components/login-form/login-form.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { SnackbarService } from 'src/app/shared/services/snackbar.service';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { delay } from 'rxjs';
+import { delay, distinctUntilChanged } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit, AfterViewInit {
+  private readonly _breakpoint$ = this._breakpointObserver
+    .observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+    .pipe(distinctUntilChanged());
+  cardWidthPercents = 66;
   passwordHidden = true;
 
   formGroup: FormGroup = new FormGroup({
@@ -20,11 +25,20 @@ export class LoginFormComponent {
   });
 
   constructor(
+    private _breakpointObserver: BreakpointObserver,
     private _userService: UserService,
     private _snackbarService: SnackbarService,
     private _router: Router,
     private _translate: TranslateService
   ) {}
+
+  ngOnInit() {
+    this._breakpoint$.subscribe(() => this.onBreakpointChange());
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.onBreakpointChange(), 0);
+  }
 
   get username() {
     return this.formGroup.get('username')?.value;
@@ -60,5 +74,21 @@ export class LoginFormComponent {
           });
       },
     });
+  }
+
+  private onBreakpointChange() {
+    const isSmall = this._breakpointObserver.isMatched(Breakpoints.Small);
+    const isMedium = this._breakpointObserver.isMatched(Breakpoints.Medium);
+    const isLarge = this._breakpointObserver.isMatched(Breakpoints.Large);
+
+    if (isSmall) {
+      this.cardWidthPercents = 80;
+    }
+    if (isMedium) {
+      this.cardWidthPercents = 66;
+    }
+    if (isLarge) {
+      this.cardWidthPercents = 33;
+    }
   }
 }
