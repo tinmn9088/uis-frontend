612b80f108ecbf7f8c8f51dbb3ed4e591e952a68
tinmn9088 <tinman.8.9@yandex.ru>
2023-04-09 03:44:32 +0300

    feat(specialization): edit button in tree node extension panel

--- src/app/specialization/components/specialization-tree-node/specialization-tree-node.component.spec.ts
+++ src/app/specialization/components/specialization-tree-node/specialization-tree-node.component.spec.ts
@@ -4,6 +4,8 @@ import { SpecializationTreeNodeComponent } from './specialization-tree-node.comp
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SpecializationFlatNode } from '../../domain/specialization-flat-node';
 import { RouterTestingModule } from '@angular/router/testing';
+import { JWT_HELPER_SERVICE_TOKEN } from 'src/app/auth/auth.module';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 describe('SpecializationTreeNodeComponent', () => {
   let component: SpecializationTreeNodeComponent;
@@ -13,6 +15,12 @@ describe('SpecializationTreeNodeComponent', () => {
     await TestBed.configureTestingModule({
       imports: [SharedModule, RouterTestingModule],
       declarations: [SpecializationTreeNodeComponent],
+      providers: [
+        {
+          provide: JWT_HELPER_SERVICE_TOKEN,
+          useValue: new JwtHelperService(),
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SpecializationTreeNodeComponent);
